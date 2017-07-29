70a8c00a4b8de439644782c19d8a131d6790b0e4
cherry <991939332@qq.com>
2017-07-29 23:29:31 +0800

    写一个js插件

--- copy-canvas-start/canvasstar.js
+++ copy-canvas-start/canvasstar.js
@@ -1,7 +1,31 @@
-;(function(global,undefined) {
-    //编写一个js插件
-    var plugin = {
+var nameSpance= window.nameSpance|| {};
 
-    }
 
-})(window);
+nameSpance.canvasStar=(function(){
+    //这里可以存放私有属性和私有方法,不对外开放，防止其他开发者私自改动
+    var privateMethod = {
+        paraHandling:function(option){
+            var configTemp ;
+            if(option.config != undefined){  //开发者设置了值
+                option.config.imgArr== undefined ? option.config.imgArr =config.imgArr:null;
+                option.config.imgSecond== undefined ? option.config.imgSecond = config.imgSecond:null;
+                option.config.isRandom== undefined ? option.config.isRandom = config.isRandom:null;
+                configTemp = option.config;
+            }else{
+                configTemp = config;
+            };
+            return configTemp;
+        },
+
+
+
+    //这里为抛出对象，供开发者使用的
+    var canvasStar = {
+        publicAttr : true,
+        publicMethod : function(){
+            console.log("这是抛出方法，供开发者使用...")
+        }
+    };
+
+    return publicObject;
+})();
